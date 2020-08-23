27c93f61a2acd562aeefa8080fbcbe6fd8e19a3a
matthieu faugere <matthieu.faugere@lifen.fr>
2020-08-23 16:44:06 +0200

    Price per day

--- server.ts
+++ server.ts
@@ -5,30 +5,144 @@ import {
   Size,
   InboundShipment,
 } from "./types";
-import { isBefore, isEqual, compareAsc, parseISO, compareDesc } from "date-fns";
+import {
+  isBefore,
+  isEqual,
+  parseISO,
+  compareDesc,
+  differenceInCalendarDays,
+} from "date-fns";
 import { dataToDescription } from "./description";
 import { formatDate, getNextDay } from "./date";
+
+const priceTable: Record<number, Record<string, number>> = {
+  1: {
+    XS: 0,
+    S: 0,
+    M: 0,
+    L: 0,
+  },
+  2: {
+    XS: 0.2,
+    S: 0.7,
+    M: 0.17,
+    L: 0.29,
+  },
+  3: {
+    XS: 0.3,
+    S: 0.11,
+    M: 0.19,
+    L: 0.31,
+  },
+  4: {
+    XS: 0.5,
+    S: 0.13,
+    M: 0.23,
+    L: 0.37,
+  },
+};
+
+function getDurationGroup(duration: number): number {
+  if (duration < 16) {
+    return 1;
+  } else if (duration < 183) {
+    return 2;
+  } else if (duration < 366) {
+    return 3;
+  }
+
+  return 4;
+}
+
+function calculatePrice(
+  currentDate: Date,
+  startStockDate: Date,
+  productSize: Size,
+  quantity: number
+) {
+  const duration = differenceInCalendarDays(currentDate, startStockDate);
+  const durationGroup = getDurationGroup(duration);
+  const priceForDurationAndSize = priceTable[durationGroup][productSize];
+
+  return priceForDurationAndSize * quantity;
+}
 
 type SizeByProduct = Record<string, Size>;
 
 function getInventoryPrice(
   inventory: ProductQuantity[],
-  date: Date,
-  sizeByProduct: SizeByProduct
+  inventoryDate: Date,
+  sizeByProduct: SizeByProduct,
+  stock: Stock
 ) {
+  let totalPrice = 0;
   for (let inventoryItem of inventory) {
-    console.log(inventoryItem);
-    console.log(sizeByProduct[inventoryItem.product]);
     if (!inventoryItem.product) {
       throw new Error("Missing product in inventory");
     }
-    if (!inventoryItem.quantity) {
+    if (inventoryItem.quantity == null || inventoryItem.quantity < 0) {
       throw new Error("Missing quantity in inventory");
     }
     if (!sizeByProduct[inventoryItem.product]) {
       throw new Error(`Missing size for product ${inventoryItem.product}`);
     }
+
+    if (inventoryItem.quantity === 0) {
+      continue;
+    }
+    const productSize = sizeByProduct[inventoryItem.product];
+    const productStock = stock[inventoryItem.product];
+    if (!productStock || !productStock.inboundShipments) {
+      throw new Error("no inbound shipments for product");
+    }
+    const deliveriesBeforeDate = productStock.inboundShipments.filter(
+      (shipmentDate) => isBefore(shipmentDate, inventoryDate)
+    );
+
+    if (deliveriesBeforeDate.length === 0) {
+      throw new Error("too much product before inbound shipment");
+    }
+
+    let quantityForDay = inventoryItem.quantity;
+    const priceForProduct = deliveriesBeforeDate.reduce(
+      (price: number, deliveryDate: Date) => {
+        if (quantityForDay === 0) {
+          return price;
+        }
+        const deliveryDateString = formatDate(deliveryDate);
+        const quantityForShipment = productStock.perDate[deliveryDateString];
+        if (quantityForShipment >= quantityForDay) {
+          const tmpPrice = calculatePrice(
+            inventoryDate,
+            deliveryDate,
+            productSize,
+            quantityForDay
+          );
+          price += tmpPrice;
+          quantityForDay = 0;
+        } else {
+          const tmpPrice = calculatePrice(
+            inventoryDate,
+            deliveryDate,
+            productSize,
+            quantityForShipment
+          );
+          price += tmpPrice;
+          quantityForDay -= quantityForShipment;
+        }
+        return price;
+      },
+      0
+    );
+
+    if (quantityForDay !== 0) {
+      throw new Error("Too much product in daily inventory");
+    }
+
+    totalPrice += priceForProduct;
   }
+
+  return totalPrice;
 }
 
 // {product1: {stock: {2020-11-04: 33, 2020-12-01: 22}}}
@@ -106,17 +220,27 @@ export function getTotalPrice(data: any) {
   const { fromDate, toDate } = description.billingPeriod;
   const sizeByProduct = getSizeByProduct(description.products);
   const stock = getStock(description.inboundShipments, toDate);
-  console.log(stock);
+
   let date = fromDate;
+  const pricePerDay: Record<string, number> = {};
+
   while (isBefore(date, toDate) || isEqual(date, toDate)) {
     const dateString = formatDate(date);
     const inventory: ProductQuantity[] = description.dailyInventory[dateString];
     if (!inventory) {
       throw new Error(`Missing inventory for day ${dateString}`);
     }
-    const price = getInventoryPrice(inventory, date, sizeByProduct);
+    const price: number = getInventoryPrice(
+      inventory,
+      date,
+      sizeByProduct,
+      stock
+    );
+    pricePerDay[dateString] = price;
     date = getNextDay(date);
   }
+
+  console.log(pricePerDay);
 }
 
 const data = {
@@ -128,25 +252,25 @@ const data = {
     "2020-12-30": [
       {
         product: "BGBL-TSHIRT-BLUS",
-        quantity: 32,
+        quantity: 9,
       },
       {
         product: "BGBL-TSHIRT-BLUM",
-        quantity: 3,
+        quantity: 15,
       },
       {
         product: "BGBL-TSHIRT-BLUL",
-        quantity: 3,
+        quantity: 90,
       },
     ],
     "2020-12-31": [
       {
         product: "BGBL-TSHIRT-BLUS",
-        quantity: 3,
+        quantity: 9,
       },
       {
         product: "BGBL-TSHIRT-BLUM",
-        quantity: 13,
+        quantity: 15,
       },
       {
         product: "BGBL-TSHIRT-BLUL",
@@ -161,6 +285,10 @@ const data = {
         {
           product: "BGBL-TSHIRT-BLUS",
           quantity: 3,
+        },
+        {
+          product: "BGBL-TSHIRT-BLUM",
+          quantity: 15,
         },
         {
           product: "BGBL-TSHIRT-BLUL",
